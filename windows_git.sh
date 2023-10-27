08a5198f7d299f4997f00185633402ef0461e31c
leungjky <leungjky@gmail.com>
2023-10-27 21:38:54 +0800

    20231027213853

--- windows_git.sh
+++ windows_git.sh
@@ -4,7 +4,8 @@ python auto_git.py
 current_date_time="`date +%Y%m%d%_H%M%S`"
 pushing_branch="main"
 target_branch="main"
-echo $pushing_branch
+user_name="$(cat user.txt)"
+key_token="$(cat key.txt)"
 git add .
 git commit -m $current_date_time
 git push $pushing_branch $target_branch
