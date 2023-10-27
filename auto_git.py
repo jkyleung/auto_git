3d15921050e94435aecfa3498e95168be32e0f5c
leungjky <leungjky@gmail.com>
2023-10-27 20:42:16 +0800

    20231027204216

--- auto_git.py
+++ auto_git.py
@@ -3,9 +3,9 @@ import sys
 
 if __name__=="__main__":
     filepath = "temp/content.txt"
-    with f = open(filepath, "a"):
-        sys_out = sys.stdout
-        sys.stdout = f
-        print(" ")
-        sys.stdout = sys_out
+    f = open(filepath, "a")
+    sys_out = sys.stdout
+    sys.stdout = f
+    print(" ")
+    sys.stdout = sys_out
     
--- windows_git.sh
+++ windows_git.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo Running Auto Git
 python auto_git.py
+current_date_time="`date +%Y%m%d%_H%M%S`"
 git add .
-git commit -m ""
+git commit -m $current_date_time
 git push
 echo Finished Auto Git
 read
