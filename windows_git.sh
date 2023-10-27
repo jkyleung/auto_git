987c3a6c55508cd5a19a4b289b4cb18fb7e73b68
leungjky <leungjky@gmail.com>
2023-10-27 21:21:51 +0800

    manual

--- windows_git.sh
+++ windows_git.sh
@@ -2,8 +2,10 @@
 echo Running Auto Git
 python auto_git.py
 current_date_time="`date +%Y%m%d%_H%M%S`"
+pushing_branch = "main"
+target_branch = "main"
 git add .
 git commit -m $current_date_time
-git push
+git push $pushing_branch $target_branch
 echo Finished Auto Git
 read
